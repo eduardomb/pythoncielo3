479660d2c2d7b0065d2e821c8973c50e47a51e5b
Eduardo Barbosa <eduardobarbosa81@gmail.com>
2016-10-24 16:56:32 -0200

    Atualização para a versão 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 0.0.3
+
+## Bug Fixes
+
+* Fix `Header value ... must be of type str or bytes` error
+
 # 0.0.2
 
 ## Bug Fixes
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pythoncielo3',
     packages=['pythoncielo3'],
-    version='0.0.2',
+    version='0.0.3',
     description='Python integration for Cielo API 3.0',
     author='Eduardo Barbosa',
     author_email='eduardo.barbosa@ufmg.br',
     url='https://github.com/eduardomb/pythoncielo3',
-    download_url='https://github.com/eduardomb/pythoncielo3/tarball/0.0.2',
+    download_url='https://github.com/eduardomb/pythoncielo3/tarball/0.0.3',
     keywords=['cielo', 'api', 'webservice'],
     install_requires=['requests'],
     classifiers=[
